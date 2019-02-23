db9d66545c51187d1a297521a89dd547c6ad5dd1
JuraseC <jurasec@gmail.com>
2019-02-22 20:00:06 -0600

    deleted math & myModule

--- graphql-basics/src/index.js
+++
@@ -1,9 +0,0 @@
-import name, { message, location, getGreeting } from './myModule'
-import {add, subtract} from './math'
- 
-console.log( message )
-console.log( name )
-console.log( location )
-console.log( getGreeting('Julio') )
-console.log( add(10, -2) )
-console.log( subtract(10, 2) )
--- graphql-basics/src/myModule.js
+++
@@ -1,9 +0,0 @@
-const message = "Message from myModule.js"
-const name = "JuraseC"
-const location = "México"
-
-const getGreeting = ( name ) => {
-    return `Welcome to the course ${name}`
-}
-
-export { message , name as default, location, getGreeting }

7087f624462cd6567c272e7a302d22e7e8c89f5d
JuraseC <jurasec@gmail.com>
2019-02-23 00:04:46 -0600

    new fields in Query and resolvers, name, location & bio

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -4,6 +4,9 @@ import { GraphQLServer } from 'graphql-yoga'
 const typeDefs = `
     type Query {
         hello: String!
+        name: String!
+        location: String!
+        bio: String
     }
 `
 
@@ -13,7 +16,16 @@ const resolvers = {
     Query:{
         hello(){
             return 'This is my first query!'
-        }
+        },
+        name(){
+            return 'My name is Julio'
+        },
+        location(){
+            return 'México'
+        },
+        bio(){
+            return 'I\'am from México and I like to play videgames'
+        }   
     }
 }
 
